e7407192d02b400711d7c247fc12455b77669a4b
av1401 <rsabhi1401@gmail.com>
2025-02-28 17:06:22 +0530

    UC 1: Setup Address Book App with a test REST API

---
+++ src/main/java/com/addressbook/app/controller/AddressBookController.java
@@ -0,0 +1,13 @@
+package com.addressbook.app.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/addressbook")
+public class AddressBookController {
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "Address Book API is working!";
+    }
+}
