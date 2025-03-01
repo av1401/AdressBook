9178c5a1ddf98a841ae914c34640a8b4ec83e583
av1401 <rsabhi1401@gmail.com>
2025-03-01 10:10:37 +0530

    UC1: Added Lombok for auto-generating getters and setters in DTO

--- src/main/java/com/addressbook/app/dto/AddressBookDTO.java
+++ src/main/java/com/addressbook/app/dto/AddressBookDTO.java
@@ -1,13 +1,13 @@
 package com.addressbook.app.dto;
 
-import lombok.*;
+import lombok.Data;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Data  // Lombok annotation to generate getters and setters
 public class AddressBookDTO {
     private String name;
     private String email;
     private String phone;
+
+    public AddressBookDTO(String name, String email, String phone) {
+    }
 }
